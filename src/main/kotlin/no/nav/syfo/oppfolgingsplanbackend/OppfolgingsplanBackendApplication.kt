ec55d67a0f0e3447cf6ace461cb48d6d798ef357
AudunSorheim <audun.sorheim@nav.no>
2023-12-07 08:42:40 +0100

    Initial Spring and nais setup

---
+++ src/main/kotlin/no/nav/syfo/oppfolgingsplanbackend/OppfolgingsplanBackendApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.syfo.oppfolgingsplanbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OppfolgingsplanBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<OppfolgingsplanBackendApplication>(args = args)
+}
