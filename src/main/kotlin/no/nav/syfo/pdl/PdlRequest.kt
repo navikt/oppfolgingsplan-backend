e39202bac91a3eef9cebcf42ea4b1eedd150c9a1
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2024-06-05 14:34:51 +0200

    Add person controller without fnr in path (#48)

---
+++ src/main/kotlin/no/nav/syfo/pdl/PdlRequest.kt
@@ -0,0 +1,16 @@
+package no.nav.syfo.pdl
+
+data class PdlRequest(
+    val query: String,
+    val variables: Variables
+)
+
+data class Variables(
+    val ident: String,
+    val grupper: String = IdentType.FOLKEREGISTERIDENT.name,
+    val navnHistorikk: Boolean = false
+)
+
+enum class IdentType {
+    FOLKEREGISTERIDENT,
+}
