f6ef0b33fad3311c9c38ac2e0e905eaa79f5dce9
malazalkoj <malaz.alkoj@nav.no>
2024-08-20 14:28:45 +0200

    Add controller to test in dev.

---
+++ src/main/kotlin/no/nav/syfo/aareg/service/ArbeidsforholdController.kt
@@ -0,0 +1,41 @@
+package no.nav.syfo.aareg.service
+
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.syfo.aareg.model.Stilling
+import no.nav.syfo.auth.tokenx.TokenXUtil.TokenXIssuer.TOKENX
+import no.nav.syfo.util.NAV_PERSONIDENT_HEADER
+import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+
+@RestController
+@ProtectedWithClaims(issuer = TOKENX, claimMap = ["acr=Level4", "acr=idporten-loa-high"], combineWithOr = true)
+@RequestMapping(value = ["/api/v1/arbeidsforhold"])
+class ArbeidsforholdController(
+    private val arbeidsforholdService: ArbeidsforholdService,
+) {
+    private val log = LoggerFactory.getLogger(ArbeidsforholdController::class.java)
+
+    @ResponseBody
+    @GetMapping(produces = [APPLICATION_JSON_VALUE])
+    fun getArbeidstakersStillingerForOrgnummer(
+        @RequestHeader(NAV_PERSONIDENT_HEADER) aktorId: String,
+        @RequestParam date: LocalDate,
+        @RequestParam orgnummer: String
+    ): ResponseEntity<List<Stilling>> {
+        /*val innloggetFnr =
+            TokenXUtil.validateTokenXClaims(contextHolder, oppfolgingsplanClientId).fnrFromIdportenTokenX().value
+*/
+        val stillinger = arbeidsforholdService.arbeidstakersStillingerForOrgnummer(aktorId, date, orgnummer)
+        log.info("Hentet ${stillinger.size} stillinger for aktorId $aktorId, orgnummer $orgnummer")
+        stillinger.forEach { log.info("Stilling: {${it.orgnummer}, ${it.fom}, ${it.tom}, ${it.yrke}, ${it.prosent}") }
+        return ResponseEntity.ok(stillinger)
+    }
+}
